eb8721c12a5e2d785f6d39861d2c806a77686fd2
Nishant <nishantpandey1711@gmail.com>
2026-06-04 11:21:47 +0530

    Updated Resource File

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "exampl4" {
+  name     = "example-resources4"
+  location = "West Europe"
+}
+
+
